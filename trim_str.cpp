a1b098879b4ee5275ed8b4f5ef7651c586bdaf1f
Besic Benjamin <besic.beni@gmx.at>
2018-12-03 12:10:49 +0100

    corrected

--- trim_str.cpp
+++ trim_str.cpp
@@ -4,20 +4,22 @@
 
 int main(int argc, char const *argv[]) {
 
-  char test_string[512];
-  strcpy(test_string,*argv);
-
   printf("\n");
   for(int i=0;i<argc;i++)
   {
     printf("%s", argv[i]);
   }
-
   printf("\n");
   printf("\n");
 
-  trim(test_string,test_string);
-  printf("%s\n",test_string);
+  char full_console_argument[512];
+  strcpy(full_console_argument,*argv);
+
+  char second_console_argument[512];
+  strcpy(second_console_argument,argv[1]);
+
+  trim(second_console_argument,second_console_argument);
+  printf("%s %s\n",argv[0],second_console_argument);
 
   return 0;
 }
